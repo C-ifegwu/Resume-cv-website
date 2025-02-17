07248019989eab1ac626d4aca786bea1ce28c57d
C.ifegwu <C.ifegwu@alustudent.com>
2025-02-18 01:25:18 +0200

    My portfolio website

---
+++ resume.js
@@ -0,0 +1,11 @@
+/* Function to open the side menu */
+function openmenu() {
+    document.getElementById("sidemenu").style.display = "flex";
+}
+
+/* Function to close the side menu */
+function closemenu() {
+    document.getElementById("sidemenu").style.display = "none";
+}
+
+/* Additional interactivity and form validations can be added here if needed */
